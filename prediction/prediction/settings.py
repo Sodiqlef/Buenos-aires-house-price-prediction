197b1d429cd0e553bbc0f8922b5aa743b7359c3b
sodiqlef <kehindeogundimu731@gmail.com>
2025-04-05 19:53:59 +0100

    security commit

--- prediction/prediction/settings.py
+++ prediction/prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .token import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$+whx&dc2vhc^ylosdon5^0ude4i6n@ze_mp+(lu2(k^=#k48'
+SECRET_KEY = SECRET_KEY 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
