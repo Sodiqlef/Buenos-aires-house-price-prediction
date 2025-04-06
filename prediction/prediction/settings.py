edd29844d13189472bf88a37488fe4d30207ad06
sodiqlef <kehindeogundimu731@gmail.com>
2025-04-06 19:22:35 +0100

    adding requirements

--- prediction/prediction/settings.py
+++ prediction/prediction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .token import SECRET_KEY
 
@@ -56,7 +57,8 @@ ROOT_URLCONF = 'prediction.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')
+],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
